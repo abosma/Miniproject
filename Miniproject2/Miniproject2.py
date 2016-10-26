3b0476e9e6a4c37f1a4e0493f536d2f926c70c37
AtillaBosma <atillabosma@hotmail.com>
2016-10-27 01:39:19 +0200

    input checking en error handling werkt nu

--- Miniproject2/Miniproject2.py
+++ Miniproject2/Miniproject2.py
@@ -1,18 +1,6 @@
-#from TwitterAPI import TwitterAPI
-#api = TwitterAPI("tdErmVotisBBi0qgClWPvC2zD", "UUNAAxEqqHbBgWOLKG6ZoWvZj1fcYuDZrc7VrmaMqJnfKiY7s5", "791235928564039680-e1gd5nWxWkObyGM5V9wMhqp6AmgCOJF", "b6Ha49AWq3KPAASIxQUbqn83OtE4KVOOcnJhp3gsIPcUL")
+from TwitterAPI import TwitterAPI
+api = TwitterAPI("tdErmVotisBBi0qgClWPvC2zD", "UUNAAxEqqHbBgWOLKG6ZoWvZj1fcYuDZrc7VrmaMqJnfKiY7s5", "791235928564039680-e1gd5nWxWkObyGM5V9wMhqp6AmgCOJF", "b6Ha49AWq3KPAASIxQUbqn83OtE4KVOOcnJhp3gsIPcUL")
 
-#file = "C:/Users/User/documents/visual studio 2015/Projects/Miniproject/Miniproject/Tweets.txt";
-
-#while True:
-#    tweets = [line.rstrip("\n") for line in open(file)];
-#    for item in tweets:
-#        print(item)
-#        a = str(input("Accept of Reject: "))
-#        if(a == "Accept"):
-#            r = api.request('statuses/update', {'status':item})
-#            print("Tweet gepost: " + item);
-#        elif(a == "Reject"):
-#            continue;
 
 import time
 def follow(thefile):
@@ -25,7 +13,22 @@ def follow(thefile):
         yield line
 
 if __name__ == '__main__':
-    logfile = open("C:/Users/User/documents/visual studio 2015/Projects/Miniproject/Miniproject/Tweets.txt","r")
+    logfile = open("D:/GithubProjects/atillabosma/Miniproject/Miniproject2/Tweets.txt","r")
     loglines = follow(logfile)
     for line in loglines:
-        print(line);
+        print(line)
+        while True:
+            a = input("Accept of Reject: ")
+            try:
+                if a == "Accept":
+                    a = str(a);
+                    r = api.request('statuses/update', {'status':line})
+                    print("Tweet gepost: " + line);
+                    break;
+                if a == "Reject":
+                    break;
+                else:
+                    print("Geen accept of reject gedecteerd, probeer het nog eens.")
+            except ValueError:
+                print("Geen nummers of tekens aub");
+                continue
