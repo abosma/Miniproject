1b4d99c4a772d99301f21d556e46044350aa112f
AtillaBosma <atillabosma@hotmail.com>
2016-10-31 13:12:25 +0100

    acceptReject tweets in functies gezet

--- Miniproject2/acceptRejectTweets.py
+++ Miniproject2/acceptRejectTweets.py
@@ -1,6 +1,7 @@
 from TwitterAPI import TwitterAPI
 import time
 
+lineTweet = None;
 api = TwitterAPI("qaDlX0kYOQ99OhOrFHDc9li07", "cLhI4Oe8D6grpDhns1udDaIxfp3SNMqECVOvHZU27mrFTR1to0", "791235928564039680-960nSheLDOtSIKiK7wwV0C7dsjQfTRb", "HZn6PJ8VyHNpuNzrx3xP4YyHJdcubqLwwqOAy8f4ji0H7")
 
 def follow(file):
@@ -13,34 +14,27 @@ def follow(file):
         yield line
 
 
-def acceptReject():
-    if a == "Accept":
-        a = str(a);
-        r = api.request('statuses/update', {'status':line})
-        print("Tweet gepost: " + line);
-        return True;
-    if a == "Reject":
-        line = line.replace("\n", "");
-        f = open("Log.txt", "a");
-        f.write(line + " : " + time.strftime("%a %d %b %Y, %T \n"));
-        f.close();
-        return True;
-    else:
-        print("Geen accept of reject gedecteerd, probeer het nog eens.")
-        return False;
+def acceptTweet():
+    logfile = open("C:/Users/User/Documents/Visual Studio 2015/Projects/Miniproject2/Miniproject2/Tweets.txt","r")
+    loglines = follow(logfile)
+    for line in loglines:
+        lineTweet = line;
+        while True:
+            r = api.request('statuses/update', {'status':line})
+            lineTweet = "Tweet gepost: " + lineTweet;
+            break;
 
-logfile = open("C:/Users/User/Documents/Visual Studio 2015/Projects/Miniproject2/Miniproject2/Tweets.txt","r")
-loglines = follow(logfile)
-for line in loglines:
-    while True:
-        print(line);
-        a = input("Accept of Reject: ")
-        try:
-            if acceptReject() == True:
-                break;
-            else:
-                continue;
-        except ValueError:
-            print("Geen nummers of tekens aub.");
-            continue
->>>>>>> origin/master
+def rejectTweet():
+    logfile = open("C:/Users/User/Documents/Visual Studio 2015/Projects/Miniproject2/Miniproject2/Tweets.txt","r")
+    loglines = follow(logfile)
+    for line in loglines:
+        lineTweet = line;
+        while True:
+            line = line.replace("\n", "");
+            f = open("Log.txt", "a");
+            f.write(line + " : " + time.strftime("%a %d %b %Y, %T \n"));
+            f.close();
+            break;
+
+
+rejectTweet();
