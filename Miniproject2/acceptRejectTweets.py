36dd65e1bd897080c6dc24c75210a3121285b07f
AtillaBosma <atillabosma@hotmail.com>
2016-10-31 13:36:10 +0100

    Accept Reject tweets 2.0

--- Miniproject2/acceptRejectTweets.py
+++ Miniproject2/acceptRejectTweets.py
@@ -1,8 +1,10 @@
 from TwitterAPI import TwitterAPI
 import time
 
-lineTweet = None;
 api = TwitterAPI("qaDlX0kYOQ99OhOrFHDc9li07", "cLhI4Oe8D6grpDhns1udDaIxfp3SNMqECVOvHZU27mrFTR1to0", "791235928564039680-960nSheLDOtSIKiK7wwV0C7dsjQfTRb", "HZn6PJ8VyHNpuNzrx3xP4YyHJdcubqLwwqOAy8f4ji0H7")
+
+global tweetStatus;
+global lineTweet;
 
 def follow(file):
     file.seek(0,2)
@@ -13,28 +15,20 @@ def follow(file):
             continue
         yield line
 
+def acceptTweet(lineTweet):
+    r = api.request('statuses/update', {'status':lineTweet})
+    tweetStatus = "Tweet gepost: " + lineTweet;
 
-def acceptTweet():
+def rejectTweet(lineTweet):
+    lineTweet = lineTweet.replace("\n", "");
+    f = open("Log.txt", "a");
+    f.write(lineTweet + " : " + time.strftime("%a %d %b %Y, %T \n"));
+    f.close();
+    tweetStatus = "Tweet opgeslagen in de log";
+
+if __name__ == '__main__':
     logfile = open("C:/Users/User/Documents/Visual Studio 2015/Projects/Miniproject2/Miniproject2/Tweets.txt","r")
     loglines = follow(logfile)
     for line in loglines:
-        lineTweet = line;
         while True:
-            r = api.request('statuses/update', {'status':line})
-            lineTweet = "Tweet gepost: " + lineTweet;
-            break;
-
-def rejectTweet():
-    logfile = open("C:/Users/User/Documents/Visual Studio 2015/Projects/Miniproject2/Miniproject2/Tweets.txt","r")
-    loglines = follow(logfile)
-    for line in loglines:
-        lineTweet = line;
-        while True:
-            line = line.replace("\n", "");
-            f = open("Log.txt", "a");
-            f.write(line + " : " + time.strftime("%a %d %b %Y, %T \n"));
-            f.close();
-            break;
-
-
-rejectTweet();
+            lineTweet = line;
