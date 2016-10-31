7877a9a4c04db8595683c07f5e8e3268c75e4dcf
wenfrie <wenfriehu@hotmail.com>
2016-10-31 12:39:22 +0100

    Gui 1.0
    add of basic buttons and teskst

--- Miniproject2/acceptRejectTweets.py
+++ Miniproject2/acceptRejectTweets.py
@@ -1,11 +1,35 @@
 from TwitterAPI import TwitterAPI
 import time
+from tkinter import *
+root = Tk() #GUI   #log erbij if time premits
+
+#Labels
+thelabel = Label(root, text="tweet")  #teskst
+
+#frames
+topFrame = Frame(root)
+bottomFrame = Frame(root)
+
+#buttons
+button1 = Button(bottomFrame, text="Accept", fg="green")  #button Accept
+button2 = Button(bottomFrame, text="Reject", fg="red")  #button REject
+
+#packs
+thelabel.pack()
+topFrame.pack()
+bottomFrame.pack(side=BOTTOM)
+button1.pack(side=LEFT)
+button2.pack(side=RIGHT)
+
+
+root.mainloop() #Main
 
 api = TwitterAPI("tdErmVotisBBi0qgClWPvC2zD", "UUNAAxEqqHbBgWOLKG6ZoWvZj1fcYuDZrc7VrmaMqJnfKiY7s5", "791235928564039680-e1gd5nWxWkObyGM5V9wMhqp6AmgCOJF", "b6Ha49AWq3KPAASIxQUbqn83OtE4KVOOcnJhp3gsIPcUL")
 
 def follow(thefile):
     thefile.seek(0,2)   #when file opened - if 0,2 go to last line
     while True:
+
         line = thefile.readline()  #twitter tweets
         if not line:
             time.sleep(0.1) #Delay 0.1s
@@ -13,10 +37,12 @@ def follow(thefile):
         yield line
 
 if __name__ == '__main__':
-    logfile = open("C:/Users/User/Documents/Visual Studio 2015/Projects/Miniproject2/Miniproject2/Tweets.txt","r") #path
+    logfile = open("C:/Users/Wenfrie/PycharmProjects/Miniproject/Miniproject2/Tweets.txt","r") #path
     loglines = follow(logfile)
     for line in loglines:
         while True:
+            print(line);
+
             a = input("Accept of Reject: ")
             try:
                 if a == "Accept":   #accept the tweet
