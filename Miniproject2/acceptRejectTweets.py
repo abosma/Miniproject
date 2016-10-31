f526d44908c8b2d9520d28e41009623dc3d92694
AtillaBosma <atillabosma@hotmail.com>
2016-10-31 12:45:20 +0100

    acceptRejectTweets in een functie gezet en weer/windsnelheid wordt nu weergeven als er geen nieuwe tweets zijn

--- Miniproject2/acceptRejectTweets.py
+++ Miniproject2/acceptRejectTweets.py
@@ -1,37 +1,44 @@
 from TwitterAPI import TwitterAPI
 import time
 
-api = TwitterAPI("tdErmVotisBBi0qgClWPvC2zD", "UUNAAxEqqHbBgWOLKG6ZoWvZj1fcYuDZrc7VrmaMqJnfKiY7s5", "791235928564039680-e1gd5nWxWkObyGM5V9wMhqp6AmgCOJF", "b6Ha49AWq3KPAASIxQUbqn83OtE4KVOOcnJhp3gsIPcUL")
+api = TwitterAPI("qaDlX0kYOQ99OhOrFHDc9li07", "cLhI4Oe8D6grpDhns1udDaIxfp3SNMqECVOvHZU27mrFTR1to0", "791235928564039680-960nSheLDOtSIKiK7wwV0C7dsjQfTRb", "HZn6PJ8VyHNpuNzrx3xP4YyHJdcubqLwwqOAy8f4ji0H7")
 
-def follow(thefile):
-    thefile.seek(0,2)
+def follow(file):
+    file.seek(0,2)
     while True:
-        line = thefile.readline()
+        line = file.readline()
         if not line:
             time.sleep(0.1)
             continue
         yield line
 
-if __name__ == '__main__':
-    logfile = open("C:/Users/User/Documents/Visual Studio 2015/Projects/Miniproject2/Miniproject2/Tweets.txt","r")
-    loglines = follow(logfile)
-    for line in loglines:
-        while True:
-            a = input("Accept of Reject: ")
-            try:
-                if a == "Accept":
-                    a = str(a);
-                    r = api.request('statuses/update', {'status':line})
-                    print("Tweet gepost: " + line);
-                    break;
-                if a == "Reject":
-                    line = line.replace("\n", "");
-                    f = open("Log.txt", "a");
-                    f.write(line + " : " + time.strftime("%a %d %b %Y, %T \n"));
-                    f.close();
-                    break;
-                else:
-                    print("Geen accept of reject gedecteerd, probeer het nog eens.")
-            except ValueError:
-                print("Geen nummers of tekens aub.");
-                continue
+def acceptReject():
+    if a == "Accept":
+        a = str(a);
+        r = api.request('statuses/update', {'status':line})
+        print("Tweet gepost: " + line);
+        return True;
+    if a == "Reject":
+        line = line.replace("\n", "");
+        f = open("Log.txt", "a");
+        f.write(line + " : " + time.strftime("%a %d %b %Y, %T \n"));
+        f.close();
+        return True;
+    else:
+        print("Geen accept of reject gedecteerd, probeer het nog eens.")
+        return False;
+
+logfile = open("C:/Users/User/Documents/Visual Studio 2015/Projects/Miniproject2/Miniproject2/Tweets.txt","r")
+loglines = follow(logfile)
+for line in loglines:
+    while True:
+        print(line);
+        a = input("Accept of Reject: ")
+        try:
+            if acceptReject() == True:
+                break;
+            else:
+                continue;
+        except ValueError:
+            print("Geen nummers of tekens aub.");
+            continue
