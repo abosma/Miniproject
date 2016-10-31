c9e6c553c81217f36e185398c567cea831e24a7b
wenfrie <wenfriehu@hotmail.com>
2016-10-31 19:53:45 +0100

    Gui 1.7(2)
    syncing

--- Miniproject2/test2.0(settweet).py
+++ Miniproject2/test2.0(settweet).py
@@ -1,26 +1,26 @@
 from tkinter import *
+from tkinter import messagebox
 root = Tk()
 filename = None
 
 def Tweet():
     global filename
     tweet =  text.get(0.0, END)    # read tweet
-    if (len(tweet) > 140):
-        tkinter.messagebox.showinfo('Error', "Meer dan 140 characters ingevoerd, typ minder dan 140 characters in.")
-        print("Meer dan 140 characters ingevoerd, typ minder dan 140 characters in."); # moet popup wroden
-    elif (len(tweet) <= 0):
-        print("Geen text ingevoerd, typ AUB iets in"); # moet popup wroden
+    if (len(tweet) > 140): # check on tweet
+        messagebox.showerror('Error', "Meer dan 140 characters ingevoerd, typ minder dan 140 characters in.") # error if len is > 140
+    elif (len(tweet) <= 0): # check on tweet
+        messagebox.showerror('Error', "Geen text ingevoerd, typ AUB iets in.") # error if no text input
     else:
-        f = open("Tweets.txt", "a");
-        f.write(tweet + "\n");
-        text.delete(0.0, END)
-        f.close();
+        f = open("Tweets.txt", "a"); # edits file
+        f.write(tweet + "\n"); # writest the file
+        text.delete(0.0, END) #ereases text so it resets
+        f.close(); # end of line
 
 
-root.title("Tweet")
+root.title("Tweet") #  GUI NAME
 
-root.minsize(width=400,height=400)
-root.maxsize(width=1000,height=1000)
+root.minsize(width=400,height=410)  #min width
+root.maxsize(width=400,height=410) # max Width
 
 text = Text(root)
 button1 = Button(text="Submit", fg="green", command=Tweet)  #Tweet button
