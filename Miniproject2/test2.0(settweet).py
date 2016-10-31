01dff407c45bc1df2d8e46c8cd5644a056e4689b
wenfrie <wenfriehu@hotmail.com>
2016-10-31 17:43:23 +0100

    GUI1.5
    AYY LMAO WORKING :)
    You can now tweet through the text box.
    note if you write more then 140 chars it will fail... (fix soon tm)

--- Miniproject2/test2.0(settweet).py
+++ Miniproject2/test2.0(settweet).py
@@ -29,18 +29,3 @@ text.pack()
 button1.pack()
 
 root.mainloop() # the window is now displayed
-
-#while True:
- #   a = input("Typ jouw tweet in: ");
-  #  if(len(a) > 140):
-  #      print("Meer dan 140 characters ingevoerd, typ minder dan 140 characters in.");
-   #     continue;
-   # elif(len(a) <= 0):
-  #      print("Geen text ingevoerd, typ AUB iets in");
-  #      continue;
-  #  else:
-  #      f = open("Tweets.txt", "a");
-  #      f.write(a + "\n");
-   #     f.close();
-
-
